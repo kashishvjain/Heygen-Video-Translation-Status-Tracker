55523f37d9c65de3552f4d89a1030c3d9bbd9a8d
Kashish Jain <kashishjain32@gmail.com>
2024-12-06 11:41:38 -0800

    Added pydoc and fixed linting

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
-import random
 import time
+import random
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
@@ -20,6 +21,13 @@ class TranslationStatus:
         self.status = "pending"
 
     def get_status(self):
+        """
+        Return the status of the job. If the job has completed, also sets status
+        to "completed" or "error" randomly with probability given by error_probability.
+
+        Returns:
+            str: "pending", "completed", or "error"
+        """
         elapsed_time = time.time() - self.started_at
 
         if elapsed_time >= self.completion_time:
@@ -36,8 +44,22 @@ server = TranslationStatus()
 
 @app.route("/status")
 def get_status():
+    """
+    Return the status of the translation job as a JSON object.
+
+    Returns:
+        JSON object with key "result" mapping to the status of the job
+            ("pending", "completed", or "error")
+    """
+
     return jsonify({"result": server.get_status()})
 
 
 def run_server(port=5000):
+    """
+    Run the Flask server on the given port.
+
+    Args:
+        port (int): The port to run the server on. Defaults to 5000.
+    """
     app.run(port=port)
